b3c548e5fe8ce0a64df70a4f4772e4c612e0fe60
Itzac <itzacalbertin@gmail.com>
2021-12-18 18:12:53 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const width = 10;
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+];
 
 function App() {
+  const [currentColorArrengement, setCurrentColorArrengement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement =[];
+
+    for(let i = 0; i < width*width; i++) {
+      const randomNum0to5 = Math.floor(Math.random() * candyColors.length);
+      const randomColor = candyColors[randomNum0to5];
+
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrengement(randomColorArrangement);
+  }
+
+  useEffect(() => {
+    createBoard();
+  }, [])
+
+  console.log(currentColorArrengement);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='candy-crush'>
+        {currentColorArrengement.map((candyColor, index : number) => (
+          <img 
+            key={index}
+            style={{backgroundColor: candyColor}}  
+          />
+        ))}
+      </div>
     </div>
   );
 }
